fa436d4c86da1873d0b839c3a3da78561422574d
Tom Ginsberg <tomginsberg@cs.toronto.edu>
2023-02-14 17:15:45 -0500

    improved documentation and power curve API

--- detectron_xgb/benchmarking.py
+++ detectron_xgb/benchmarking.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import numpy as np
+import pandas as pd
 import xgboost as xgb
 from tqdm import tqdm
 
@@ -17,26 +18,27 @@ def detectron_test_statistics(
         sample_size: int,
         xgb_params=XGB_PARAMS,
         ensemble_size=10,
-        calibration_runs=100,
+        num_runs=100,
         num_boost_round=10,
         patience=3,
         balance_train_classes=True
 ):
     """
     Run the Detectron algorithm for `seeds` times, and return
-    :param train:
-    :param val:
-    :param test:
-    :param base_model:
-    :param sample_size:
-    :param xgb_params: (defaults.XGB_PARAMS) parameters for the XGBoost model
+    :param train: tuple of (data, labels) for the training data
+    :param val: tuple of (data, labels) for the validation data
+    :param test: tuple of (data, labels) for the test data
+    :param base_model: the base model to use for the pseudo-labeling, this should be a trained XGBoost model
+    :param sample_size: the number of samples to use from the test set on each random run
+    :param xgb_params: (detectron_xgb.defaults.XGB_PARAMS) parameters for the XGBoost model
     :param ensemble_size: (10) the number of models in the ensemble
-    :param calibration_runs: (100) the number of different random runs to perform,
-        each run operates on a random sample from q
+    :param num_runs: (100) the number of different random runs to perform,
+        each run operates on a random sample from test
     :param num_boost_round: (10) xgb parameter for the number of boosting rounds
     :param patience: (3) number of ensemble rounds to wait without improvement in the rejection rate
     :param balance_train_classes: (True) If True, the training data will be automatically balanced using weights.
     :return: XGBDetectronRecord object containing all the information of this run
+        see detectron_xgb.utils.data.XGBDetectronRecord
     """
     record = XGBDetectronRecord(sample_size)
 
@@ -48,7 +50,7 @@ def detectron_test_statistics(
     # set up the validation data
     val_data = xgb.DMatrix(val_data, val_labels)
 
-    for seed in tqdm(range(calibration_runs)):
+    for seed in tqdm(range(num_runs)):
 
         # randomly sample N elements from q
         idx = np.random.RandomState(seed).permutation(len(test[0]))[:sample_size]
@@ -139,4 +141,18 @@ def detectron_dis_power(calibration_record: XGBDetectronRecord,
 
     return dict(power=tpr_at_alpha, auc=np.trapz(tpr, fpr), N=N)
 
-# def detectron_dis_power_curve(train, val, iid_test, ood_test)
+
+def detectron_dis_power_curve(train, val, iid_test, ood_test, base_model,
+                              sample_sizes, alpha=0.05, max_ensemble_size=None,
+                              **detectron_kwargs):
+    res = []
+    for N in sample_sizes:
+        print(f'Running Detectron with N={N}')
+        cal_record = detectron_test_statistics(train, val, iid_test, base_model=base_model, sample_size=N,
+                                               **detectron_kwargs)
+        test_record = detectron_test_statistics(train, val, ood_test, base_model=base_model, sample_size=N,
+                                                **detectron_kwargs)
+        res.append(
+            detectron_dis_power(cal_record, test_record, alpha=alpha, max_ensemble_size=max_ensemble_size) | dict(N=N))
+
+    return pd.DataFrame(res)
